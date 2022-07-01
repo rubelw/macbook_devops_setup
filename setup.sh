e5ec490bbf627153fbf035b6ba139088290fa1a3
rubelw <willrubel@gmail.com>
2022-07-01 18:52:08 -0500

    Add git

--- setup.sh
+++ setup.sh
@@ -11,42 +11,42 @@ pretty_print() {
 # 
 pretty_print "Here we go..."
 
-# So it begins
+if ! [ -x "$(command -v brew)" ]; then
 
+    # Homebrew installation
 
-# Homebrew installation
+    if ! command -v brew &>/dev/null; then
+      pretty_print "Installing Homebrew, an OSX package manager, follow the instructions..."
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-if ! command -v brew &>/dev/null; then
-  pretty_print "Installing Homebrew, an OSX package manager, follow the instructions..." 
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      if ! grep -qs "recommended by brew doctor" ~/.zshrc; then
+        pretty_print "Put Homebrew location earlier in PATH ..."
+          printf '\n# recommended by brew doctor\n' >> ~/.zshrc
+          printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
+          export PATH="/usr/local/bin:$PATH"
+      fi
+    else
+      pretty_print "You already have Homebrew installed...good job!"
+    fi
 
-  if ! grep -qs "recommended by brew doctor" ~/.zshrc; then
-    pretty_print "Put Homebrew location earlier in PATH ..."
-      printf '\n# recommended by brew doctor\n' >> ~/.zshrc
-      printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
-      export PATH="/usr/local/bin:$PATH"
-  fi
-else
-  pretty_print "You already have Homebrew installed...good job!"
+    # Homebrew OSX libraries
+
+    pretty_print "Updating brew formulas"
+        brew update
+
+    pretty_print "Installing GNU core utilities..."
+        brew install coreutils
+
+    pretty_print "Installing GNU find, locate, updatedb and xargs..."
+        brew install findutils
+
+    pretty_print "Installing the most recent verions of some OSX tools"
+        brew tap homebrew/dupes
+        brew install homebrew/dupes/grep
+
+    printf 'export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"' >> ~/.zshrc
+    export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 fi
-
-# Homebrew OSX libraries
-
-pretty_print "Updating brew formulas"
-  	brew update
-
-pretty_print "Installing GNU core utilities..."
-	brew install coreutils
-
-pretty_print "Installing GNU find, locate, updatedb and xargs..."
-	brew install findutils
-
-pretty_print "Installing the most recent verions of some OSX tools"
-	brew tap homebrew/dupes
-	brew install homebrew/dupes/grep
-
-printf 'export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"' >> ~/.zshrc
-export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
 
 # Install git
